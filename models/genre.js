d90c60c7ca9eb48da6413704f4d4269a44c4b4c0
WavyBoyJodii <najeecasey@gmail.com>
2023-10-10 16:45:57 -0400

    add logic for genre controller and routes

--- models/genre.js
+++ models/genre.js
@@ -6,4 +6,8 @@ GenreSchema = new Schema({
   name: { type: String, required: true, minLength: 3, maxLength: 50 },
 });
 
+GenreSchema.virtual('url').get(function () {
+  return `/genre/${this._id}`;
+});
+
 module.exports = mongoose.model('Genre', GenreSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 // Require controller modules
 const album_controller = require('../controllers/albumController');
+const genre_controller = require('../controllers/genreController');
 
 /* GET home page. */
 router.get('/', album_controller.album_list);
@@ -27,5 +28,31 @@ router.post('/album/:id/update', album_controller.album_update_post);
 
 // Get request for one Album
 router.get('/album/:id', album_controller.album_detail);
+
+/// GENRE ROUTES ///
+
+// GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
+router.get('/genre/create', genre_controller.genre_create_get);
+
+//POST request for creating Genre.
+router.post('/genre/create', genre_controller.genre_create_post);
+
+// GET request to delete Genre.
+router.get('/genre/:id/delete', genre_controller.genre_delete_get);
+
+// POST request to delete Genre.
+router.post('/genre/:id/delete', genre_controller.genre_delete_post);
+
+// GET request to update Genre.
+// router.get('/genre/:id/update', genre_controller.genre_update_get);
+
+// POST request to update Genre.
+// router.post('/genre/:id/update', genre_controller.genre_update_post);
+
+// GET request for one Genre.
+router.get('/genre/:id', genre_controller.genre_detail);
+
+// GET request for list of all Genre.
+router.get('/genres', genre_controller.genre_list);
 
 module.exports = router;
